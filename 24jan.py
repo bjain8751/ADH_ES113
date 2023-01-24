1eda9318e64c02a9c63d51d3bf00eb272a075fde
bjan8751 <bhaveshjain8751@gmail.com>
2023-01-24 23:01:41 +0530

    session 1

--- 24jan.py
+++ 24jan.py
@@ -4,33 +4,53 @@
 
 
 #bhavesh=student()
-
-class student:
-
-    def __init__(sur, name, rollno, age, marks):
-        sur.name = name
-        sur.rollno = rollno
-        sur.age = age
-        sur.marks = marks
+# class teacher:
     
-    def age_marks(self):
-        return(self.age+self.marks)
+#     def add_marksage(self):
+#         return (self.marks+self.age)
 
-    def __repr__(self):
-        return (f"Name: {self.name}, Roll No: {self.rollno}, Age: {self.age}, Marks: {self.marks}")
 
-s1=student("Bhavesh", 1, 20, 100)
-s2=student("Paras", 2, 22, 100)
+# t1=teacher()
+# t1.name="rohan"
+# t1.age=20
 
-print(s1.age_marks())
-
-print(s1)
+# t1.marks=100
 
 
 
+# class student:
 
-# def printhello():
-#     print("Hello")
+#     def __init__(sur, name, rollno, age, marks):
+#         sur.name = name
+#         sur.rollno = rollno
+#         sur.age = age
+#         sur.marks = marks
+    
+#     def age_marks(sr):
+#         return(sr.age+sr.marks)
+
+#     def __repr__(self):
+#         return (f"Name: {self.name}, Roll No: {self.rollno}, Age: {self.age}, Marks: {self.marks}")
+
+# s1=student("Bhavesh", 1, 20, 100)
+# s2=student("Paras", 2, 22, 100)
+
+# print(s1.age_marks())
+
+
+# print(s1)
+
+
+
+
+# def printhello(a):
+#     print(a)
+
+# def printh(b):
+#     print(a)
+# print(printh(34))
+
+
 
 
 # printhello("HI")
@@ -38,6 +58,49 @@ print(s1)
 #class point: (x,y,name) , distance from origin, print, distance from another point, add corresponding cordinates
 
 
+class point:
+    def __init__(self, x, y, name):
+        self.x=x
+        self.y=y
+        self.name=name
+
+    def distance_from_origin(self):
+        return (self.x**2+self.y**2)**0.5
+
+    def __repr__(self):
+        return (f"Name: {self.name}, X: {self.x}, Y: {self.y}")
+
+    def distance_from_anotherpoint(a1, a2):
+        return ((a1.x-a2.x)**2+(a1.y-a2.y)**2)**0.5
+
+    def add_corresponding_cordinates(self, a2):
+        return (self.x+a2.x, self.y+a2.y)
+    
+    def __add__(self, a2):
+        return (self.x+a2.x, self.y+a2.y)
+
+
+
+
+
+
+
+
+
+p2=point(2,3,"delhi")
+p1=point(1,2,"mumbai")
+
+print(p1.distance_from_origin())
+print(p2.distance_from_origin())
+
+print(p1)
+print(p2)
+
+print(p1.distance_from_anotherpoint(p2))
+
+print(p1.add_corresponding_cordinates(p2))
+
+print(p1+p2)
 
 
 
